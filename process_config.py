f36753a5e8cd29012c618893eaf67d9dbb126c67
Michael Bolin <mbolin@meta.com>
2024-01-29 22:51:00 -0800

    introduce CLI args that default to env vars so process_config.py can be useful outside of a GitHub action

--- process_config.py
+++ process_config.py
@@ -13,11 +13,6 @@ from functools import cache
 from typing import Any, Dict, Literal, Tuple, Union
 
 
-github_repository = os.environ["GITHUB_REPOSITORY"]
-github_server_url = os.environ["GITHUB_SERVER_URL"]
-gh_repo_arg = f"{github_server_url}/{github_repository}"
-
-
 def main() -> None:
     exit_code = _main()
     sys.exit(exit_code)
@@ -31,11 +26,31 @@ def _main() -> int:
     )
 
     args = parse_args()
-    config = get_config(args.config)
-    logging.info(json.dumps(config, indent=2))
+    repo: str = args.repo
+    if not repo:
+        raise ValueError(
+            "no repo specified: must specify --repo or set the GITHUB_REPOSITORY environment variable"
+        )
+
+    output_folder = args.output
+    if not output_folder:
+        output_folder = tempfile.mkdtemp(prefix=f"{repo.replace('/', '_')}_dotslash")
+    logging.info(f"DotSlash files will be written to `{output_folder}")
 
     tag = args.tag
-    name_to_asset = get_release_assets(tag)
+    github_server_url = args.server
+    api_server_url = args.api_server
+    gh_repo_arg = f"{github_server_url}/{repo}"
+
+    config = get_config(
+        path_to_config=args.config,
+        config_ref=args.config_ref,
+        github_repository=repo,
+        api_url=api_server_url,
+    )
+    logging.info(json.dumps(config, indent=2))
+
+    name_to_asset = get_release_assets(tag=tag, github_repository=repo)
     logging.info(json.dumps(name_to_asset, indent=2))
 
     platform_entries = map_platforms(config, name_to_asset)
@@ -45,32 +60,34 @@ def _main() -> int:
 
     logging.info(json.dumps(platform_entries, indent=2))
 
-    manifest_file_contents = generate_manifest_file(tag, platform_entries)
+    manifest_file_contents = generate_manifest_file(gh_repo_arg, tag, platform_entries)
     logging.info(manifest_file_contents)
 
     release_filename = config.get("release_filename")
     if release_filename:
-        output_file = os.path.join(os.environ["GITHUB_WORKSPACE"], release_filename)
+        output_file = os.path.join(output_folder, release_filename)
         with open(output_file, "w") as f:
             f.write(manifest_file_contents)
         logging.info(f"wrote manifest to {output_file}")
 
         # Upload manifest to release, but do not clobber. Note that this may
         # fail if this action has been called more than once for the same config.
-        subprocess.run([
-            "gh",
-            "release",
-            "upload",
-            tag,
-            output_file,
-            "--repo",
-            gh_repo_arg,
-        ])
+        subprocess.run(
+            [
+                "gh",
+                "release",
+                "upload",
+                tag,
+                output_file,
+                "--repo",
+                gh_repo_arg,
+            ]
+        )
 
     return 0
 
 
-def generate_manifest_file(tag: str, platform_entries) -> str:
+def generate_manifest_file(gh_repo_arg: str, tag: str, platform_entries) -> str:
     platforms = {}
     with tempfile.TemporaryDirectory() as temp_dir:
         for platform_name, platform_entry in platform_entries.items():
@@ -86,7 +103,7 @@ def generate_manifest_file(tag: str, platform_entries) -> str:
                 logging.error(f"missing 'name' field in asset: {asset}")
                 return 1
 
-            hash_hex = compute_hash(temp_dir, tag, name, hash_algo, size)
+            hash_hex = compute_hash(gh_repo_arg, temp_dir, tag, name, hash_algo, size)
             platform_fetch_info = {
                 "url": asset["url"],
                 "digest": {"type": hash_algo, "value": hash_hex},
@@ -163,6 +180,7 @@ def map_platforms(
 
 @cache
 def compute_hash(
+    gh_repo_arg: str,
     temp_dir: str,
     tag: str,
     name: str,
@@ -218,10 +236,9 @@ def compute_hash(
         return hasher.hexdigest()
 
 
-def get_config(path_to_config: str) -> Any:
-    api_url = os.environ["GITHUB_API_URL"]
-    ref = os.environ["GITHUB_SHA"]
-
+def get_config(
+    *, path_to_config: str, config_ref: str, github_repository: str, api_url: str
+) -> Any:
     args = [
         "gh",
         "api",
@@ -231,13 +248,13 @@ def get_config(path_to_config: str) -> Any:
         "-H",
         "Accept: application/vnd.github.raw",
         "-f",
-        f"ref={ref}",
+        f"ref={config_ref}",
     ]
     output = subprocess.check_output(args)
     return json.loads(output.decode("utf-8"))
 
 
-def get_release_assets(tag: str) -> Dict[str, Any]:
+def get_release_assets(*, tag: str, github_repository) -> Dict[str, Any]:
     args = [
         "gh",
         "release",
@@ -259,8 +276,42 @@ def get_release_assets(tag: str) -> Dict[str, Any]:
 def parse_args():
     parser = argparse.ArgumentParser(description="A simple argparse example")
 
-    parser.add_argument("--tag", help="tag identifying the release")
-    parser.add_argument("--config", help="path to JSON config file")
+    parser.add_argument("--tag", required=True, help="tag identifying the release")
+    parser.add_argument("--config", required=True, help="path to JSON config file")
+    parser.add_argument(
+        "--repo",
+        help="github repo specified in `ORG/REPO` format",
+        default=os.getenv("GITHUB_REPOSITORY"),
+    )
+
+    # It would make things slightly easier for the user to default to the
+    # default branch of the repo, which might not be main.
+    default_config_ref = "main"
+    parser.add_argument(
+        "--config-ref",
+        help=f"SHA of Git commit to look up the config, defaults to {default_config_ref}",
+        default=os.getenv("GITHUB_SHA", default_config_ref),
+    )
+
+    default_server = "https://github.com"
+    parser.add_argument(
+        "--server",
+        help=f"URL for the GitHub server, defaults to {default_server}",
+        default=os.getenv("GITHUB_SERVER_URL", default_server),
+    )
+
+    default_api_server = "https://api.github.com"
+    parser.add_argument(
+        "--api-server",
+        help=f"URL for the GitHub API server, defaults to {default_api_server}",
+        default=os.getenv("GITHUB_API_URL", default_api_server),
+    )
+
+    parser.add_argument(
+        "--output",
+        help=f"folder where DotSlash files should be written, defaults to $GITHUB_WORKSPACE",
+        default=os.getenv("GITHUB_WORKSPACE"),
+    )
 
     return parser.parse_args()
 
